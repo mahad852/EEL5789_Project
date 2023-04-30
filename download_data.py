8b96940a034dbd4236b50ceb7d3374a28a0360e0
Mahad Ali <mahad852@gmail.com>
2023-04-30 03:27:52 -0400

    using the requests library instead of urlib when downloading dataset

--- download_data.py
+++ download_data.py
@@ -1,19 +1,27 @@
-import urllib.request
+import requests
 import zipfile
 import os
 import pandas as pd
 
 DATASET_FILE_URL = 'https://iotanalytics.unsw.edu.au/iottestbed/csv/16-09-23.csv.zip'
-DATASET_FILE = '16-09-23.csv'
+DATASET_FILE = '16-09-23.csv.zip'
 
 def does_dataset_exist():
     return DATASET_FILE in os.listdir(os.path.curdir)
 
+
 def download_dataset():
-    urllib.request.urlretrieve(DATASET_FILE_URL, DATASET_FILE)
+    if DATASET_FILE_URL.split('/')[-1] is os.listdir(os.path.curdir):
+        return
+    
+    with requests.get(DATASET_FILE_URL) as r:
+        with open(DATASET_FILE_URL.split('/')[-1], 'wb') as f:
+            f.write(r.content)
+
+    return DATASET_FILE
 
 def unzip_dataset():
-    with zipfile.ZipFile(os.path.join(os.path.curdir, DATASET_FILE), 'r') as zip_ref:
+    with zipfile.ZipFile(os.path.join(os.path.curdir, DATASET_FILE_URL.split('/')[-1]), 'r') as zip_ref:
         zip_ref.extractall(os.path.curdir)
 
 
